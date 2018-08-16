c0e0df83e791a2cabc1a5e4c632aee11075f23ac
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-16 13:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine){
     return "There is nobody waiting to be served!";
   }
   else{
-    var shift = katzDeliLine.shift(0);
+    var shift = katzDeliLine.shift(1);
     return "Currently serving " + katzDeliLine.shift(1) +".";
   }
 }
