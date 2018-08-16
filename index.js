c23efe3410386ab05820a23cffd7e4c5dd22256a
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-16 13:04:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
-    return "The line is currently empty."
+    return "The line is currently empty.";
+    
   }
 }
