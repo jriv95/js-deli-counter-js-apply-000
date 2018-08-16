5a8bfffca967616d7d42c6ed7e506ebf2c1ac306
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-16 12:54:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function nowServing(katzDeliLine){
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return "Welcome," + name + ". You are number " + katzDeliLine.length + " in line."; 
+  return "Welcome," + name + ". You are number " + katzDeliLine.length + " in line.";
 }
